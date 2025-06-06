6868b31ba10b96ae3935b509cae4e4c43a99fd9f
Minseok An <chad320@korea.ac.kr>
2025-06-06 19:48:20 +0900

    Add files via upload

---
+++ process_03.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char* argv[]){
+    printf("where am I?\n");
+
+    execl("/bin/pwd","pwd", (char*)NULL);
+
+    return 0;
+}
+
+
+
+
+/*
+Expected output:
+
+Where am I?
+**some directory where process_03.c is in**
+(example: /home/dnclab/shared/process)
+*/
